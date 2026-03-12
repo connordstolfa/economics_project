24fcd145b1115a1a9f414f24120fe8a0d4f70fc5
Connor Stolfa <connorstolfa@gmail.com>
2026-03-12 15:45:06 -0700

    Committing more models and the start of my DBT project. :

--- fred_calls/duckdb_utils.py
+++ fred_calls/duckdb_utils.py
@@ -13,7 +13,19 @@ class DuckClient:
             return con.execute(sql, params).df()
         
     def upsert_data(self, df, table_name):
+        """This function checks if a table exists for a give data point.
+        If it doesn't it createds it then runs the upsert function.
+        If the table already exists, the first function won't do anythign and upsert will run."""
         df['processed_at'] = pd.Timestamp.now()
 
         with duckdb.connect(self.db_path) as con:
+            # Check if table exits and create it if it doesn't
+            con.execute(f"""
+                        CREATE TABLE IF NOT EXISTS raw.{table_name} (
+                            date TIMESTAMP PRIMARY KEY,
+                            value DOUBLE,
+                            processed_at TIMESTAMP
+                        )
+                    """)
+            # Run the upsert function.
             con.execute( f"INSERT OR REPLACE INTO raw.{table_name} SELECT * FROM df")
--- fred_calls/ingest_fred_data.py
+++ fred_calls/ingest_fred_data.py
@@ -3,26 +3,36 @@ import duckdb_utils as ddu
 import fred_constants as fc
 
 def insert_fred_data(
-            fred_series:list,
-            table_names:list,
+            # fred_series:list,
+            series_with_table_names:dict,
+            # table_names:list,
             db_path:str,
         ):
     
     fred = fc.FredClient()
     db = ddu.DuckClient(db_path)
-    print(fred, db)
 
-    for series_id, table_name in list(zip(fred_series, table_names)):
+    for series_id, table_name in series_with_table_names.items():
         # Call the Fred API and get the series you pass.
         try:
             df = fred.ingest_series(series_id)
         except ValueError as e:
             print(f"Error querying Series. Check the series name: {e}.")
-        
         # Upsert that series.
         db.upsert_data(df, table_name=table_name)
 
 
 db_path = "/home/connor/projects/economics_project/fred_data/economics.db"
 
-insert_fred_data(['UNRATE'], ['unemployment_rate'], db_path)
+series_to_pull = {
+    "UNRATE": 'unemployment_rate',
+    "CPIAUCSL": "consumer_price_index",
+    "CPILFESL": "core_consumer_price_index",
+    "PCEPI": "personal_consumption_expenditures_index",
+    "GDPC1": "real_gdp",
+    "A939RX0Q048S": "real_gdp_per_capita",
+    "RPI": "real_income_per_capita",
+    "MAPAIN": "real_mean_household_income",
+}
+
+insert_fred_data(series_to_pull, db_path)
